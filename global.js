7d25d2890ffb8695a57de656c793baa20370ca5b
Shen Nan <wongshennan@live.com>
2021-07-21 17:29:36 +0800

    base deck code + base css

---
+++ global.js
@@ -0,0 +1,18 @@
+
+// Create an empty helper function called calcHandScore that returns a fixed number of points for now, e.g. 1.
+
+  // Use JSDoc function commenting with the following attributes.
+  // Description of function
+  // Description of each parameter
+  // Description of return value
+// calcHandScore will take an array of card objects and return the number of points that the user scored for the cards in their hand.
+// Abstracting calcHandScore allows us to construct the primary game logic without worrying about hand-scoring logic.
+// It will also make testing hand-scoring logic easier because we can test scoring of individual hands without running the whole game.
+
+// Code the primary game logic using calcHandScore wherever we wish to calculate the score of a given hand. This logic should include the following.
+  // The user has a global number of points.
+  // The user clicks a button to deal cards.
+  // The user selects which cards they want to keep.
+  // The game replaces the unselected cards, calculates the hand score, and updates total points.
+// Lay out game controls for mobile (portrait orientation), and set the max-width CSS property of the container so the layout is still friendly on a wider screen.
+
---
+++ testHands.js
@@ -0,0 +1,8 @@
+// calcHandScore
+// After we have a playable version of the game, add logic to calcHandScore.
+// Add logic for detecting each hand (e.g. full-house, flush, 2 pair) 1 at a time. 
+// Test the logic for each hand before moving onto the next hand.
+// Consider using a JS Object to track frequencies of specific ranks or suits. This might make it easier to detect hands such as flushes, full houses, and X-of-a-kind. See 0.4: JS Object as Tally for an example.
+// Hard-code the arrays of card objects we will need to test our logic.
+// Put these test hands in a separate file, e.g. testHands.js.
+// Don't forget to test the negative cases, e.g. do we still win/lose the right number of points if we don't get the hand we are currently testing?
