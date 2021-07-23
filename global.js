3cbc737d961012aca645d4381586b83f19bd41ca
Shen Nan <wongshennan@live.com>
2021-07-24 00:09:16 +0800

    basic game functionality with animation

--- global.js
+++ global.js
@@ -1,49 +1,59 @@
-
 // Create an empty helper function called calcHandScore that returns a fixed number of points for now, e.g. 1.
 
-  // Use JSDoc function commenting with the following attributes.
-  // Description of function
-  // Description of each parameter
-  // Description of return value
+// Use JSDoc function commenting with the following attributes.
+// Description of function
+// Description of each parameter
+// Description of return value
 // calcHandScore will take an array of card objects and return the number of points that the user scored for the cards in their hand.
 // Abstracting calcHandScore allows us to construct the primary game logic without worrying about hand-scoring logic.
 // It will also make testing hand-scoring logic easier because we can test scoring of individual hands without running the whole game.
 
 // Code the primary game logic using calcHandScore wherever we wish to calculate the score of a given hand. This logic should include the following.
-  // The user has a global number of points.
-  // The user clicks a button to deal cards.
-  // The user selects which cards they want to keep.
-  // The game replaces the unselected cards, calculates the hand score, and updates total points.
+// The user has a global number of points.
+// The user clicks a button to deal cards.
+// The user selects which cards they want to keep.
+// The game replaces the unselected cards, calculates the hand score, and updates total points.
 // Lay out game controls for mobile (portrait orientation), and set the max-width CSS property of the container so the layout is still friendly on a wider screen.
 
-// init deck as an empty array
+// init deck and playerhand as an empty array
 let deck = [];
-
-// issue first two cards to player & first card to house
 let playerHand = [];
-let houseHand = []; //face down, face up
+
+// init helper arrays and objects
+let cardsToBeSwapped = [];
+
+// init eventListeners
+let betUp; let betDown; let deal; let
+  swap;
 
 let cardSuitTally = {};
 let cardValueTally = {};
 
+let pot = 100;
+let bet = 0;
 
-//generates deck of cards ordered by rank e.g. AAAA -> 2222 -> 3333
+// generates deck of cards ordered by rank e.g. 2222 -> 3333 ... -> AAAA
+// each card is an object with {rank, display in unicode, name of the card, suit, card value in numbers, display name}
 const generateDeck = () => {
-  const suits = ["Diamonds", "Clubs", "Hearts", "Spades"];
-  const card = ["2","3","4","5","6","7","8","9","10","Jack","Queen","King", "Ace"];
+  const suits = ['♦', '♣', '♥', '♠'];
+  const unicodeSuits = ['🃂', '🃒', '🂲', '🂢', '🃃', '🃓', '🂳', '🂣', '🃄', '🃔', '🂴', '🂤', '🃅', '🃕', '🂵', '🂥', '🃆', '🃖', '🂶', '🂦', '🃇', '🃗', '🂷', '🂧', '🃈', '🃘', '🂸', '🂨', '🃉', '🃙', '🂹', '🂩', '🃊', '🃚', '🂺', '🂪', '🃋', '🃛', '🂻', '🂫', '🃍', '🃝', '🂽', '🂭', '🃎', '🃞', '🂾', '🂮', '🃁', '🃑', '🂱', '🂡'];
+  const cardName = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  const color = ['red', 'black'];
 
   for (i = 0; i < 52; i++) {
-    var newCard = {};
+    const newCard = {};
     deck[i] = newCard;
-    newCard.rank = i+1;
-    newCard.property = card[Math.floor(i / 4)];
-    newCard.value = Math.floor(i/4)+2;
+    newCard.rank = i + 1;
+    newCard.display = unicodeSuits[i];
+    newCard.property = cardName[Math.floor(i / 4)];
+    newCard.color = color[Math.floor(i % 2)];
+    newCard.value = Math.floor(i / 4) + 2;
     newCard.suit = suits[(i + 4) % 4];
-    newCard.name = newCard.property + " of " + newCard.suit;
+    newCard.name = `${newCard.property} of ${newCard.suit}`;
   }
-}
+};
 
-//shuffle the deck
+// shuffle the deck
 const getRandomIndex = function (max) {
   return Math.floor(Math.random() * max);
 };
@@ -51,9 +61,9 @@ const getRandomIndex = function (max) {
 const shuffleCards = function (deck) {
   let currentIndex = 0;
   while (currentIndex < deck.length) {
-    let randomIndex = getRandomIndex(deck.length);
-    let randomCard = deck[randomIndex];
-    let currentCard = deck[currentIndex];
+    const randomIndex = getRandomIndex(deck.length);
+    const randomCard = deck[randomIndex];
+    const currentCard = deck[currentIndex];
     deck[currentIndex] = randomCard;
     deck[randomIndex] = currentCard;
     currentIndex += 1;
@@ -61,60 +71,141 @@ const shuffleCards = function (deck) {
   return deck;
 };
 
-generateDeck();
-shuffledDeck = shuffleCards(deck)
-playerHand = shuffledDeck.splice(0,5);
-
-const tallyHandValue = (hand) => {
-  for (let i=0; i<hand.length; i+=1) {
-    //if we have seen the card __ before, increment its count
-    let cardValue = hand[i].value;
-    if (cardValue in cardValueTally){
-      cardValueTally[cardValue] +=1
-    }
-    //else, initialise count of this card __ to 1
-    else{
-      cardValueTally[cardValue] =1;
-    }
+// behaviours when cards are selected to return
+const playerSelectCard = (cardIndex) => {
+  // eslint-disable-next-line prefer-const
+  let selectedSuit = document.getElementById(`${playerHand[cardIndex].rank}`);
+  const selectedCard = document.getElementById(`c${playerHand[cardIndex].rank}`);
+  selectedCard.classList.toggle('is-flipped');
+  if (selectedSuit.classList.contains('selected')) {
+    selectedSuit.classList.remove('selected');
+    console.log('click');
+    const deselectIndex = cardsToBeSwapped.indexOf(cardIndex);
+    cardsToBeSwapped.splice(deselectIndex, 1);
+  } else {
+    selectedSuit.classList.add('selected');
+    cardsToBeSwapped.push(cardIndex);
   }
-
-  for(cardValue in cardValueTally){
-    console.log(`There are ${cardValueTally[cardValue]} ${cardValue} in the hand`);
-  }
-}
-
-const tallyHandSuit = (hand) => {
-  for (let i=0; i<hand.length; i+=1) {
-    //if we have seen the card __ before, increment its count
-    let cardSuit = hand[i].suit;
-    if (cardSuit in cardSuitTally){
-      cardSuitTally[cardSuit] +=1
-    }
-    //else, initialise count of this card __ to 1
-    else{
-      cardSuitTally[cardSuit] =1;
-    }
-  }
-
-  for(cardSuit in cardSuitTally){
-    console.log(`There are ${cardSuitTally[cardSuit]} ${cardSuit} in the hand`);
-  }
-}
+};
 
 // swap cards takes an array of indexes of cards needing to be swapped, then swaps them with another card in the deck
-const swapCards = (array,hand) =>{
-  for(let i=0; i<array.length;i+=1){
-    let replacementCard = deck.pop();
-    hand.splice(array[i],1,replacementCard);
-    return hand;
+const swapCards = (array, hand) => {
+  console.log(array, hand);
+
+  if (array.length === 0) {
+    datafield.innerHTML = calcHandScore(hand);
+    endTurn();
+  } else {
+    for (let i = 0; i < array.length; i += 1) {
+      const selectedCard = document.getElementById(`c${playerHand[array[i]].rank}`);
+      selectedCard.classList.toggle('slide-out-blurred-top');
+
+      const selectedContainer = selectedCard.parentElement;
+      setTimeout(() => { selectedContainer.removeChild(selectedCard); }, 500);
+      const replacementCardObject = deck.pop();
+      const replacementCard = createCard(replacementCardObject, array[i]);
+      setTimeout(() => {
+        replacementCard.classList.toggle('slide-in-blurred-top');
+        selectedContainer.appendChild(replacementCard); }, 1000);
+      // eslint-disable-next-line prefer-const
+      hand.splice(array[i], 1, replacementCardObject);
+    }
+    datafield.innerHTML = calcHandScore(hand);
+    endTurn();
   }
-}
+};
 
-// comparison is a helper function.
-const comparison = (a,b) => {
-  return a.rank-b.rank;
-}
+const createCard = (object, index) => {
+  const suit = document.createElement('div');
+  suit.classList.add('suit-front', object.color);
+  suit.innerHTML = object.display;
+  suit.id = object.rank;
 
-// Sorts the playerHand by order of rank
-playerHand.sort(comparison)
+  const suitback = document.createElement('div');
+  suitback.classList.add('suit-back');
+  suitback.innerHTML = '&#127136';
+  suitback.id = object.rank;
+  suit.addEventListener('click', () => { playerSelectCard(index); });
+  suitback.addEventListener('click', () => { playerSelectCard(index); });
 
+  const card = document.createElement('div');
+  card.classList.add('cards');
+  card.id = `c${object.rank}`;
+
+  card.appendChild(suit);
+  card.appendChild(suitback);
+
+  return card;
+};
+
+// main creation function to visually display the cards from hand
+const createCardsFromArray = (hand) => {
+  for (let j = 0; j < hand.length; j += 1) {
+    const cardsContainer = document.createElement('div');
+    cardsContainer.classList.add('cardsContainer', 'slide-in-blurred-top');
+    cardsContainer.id = `cc${j}`;
+    cardsContainer.appendChild(createCard(hand[j], j));
+    setTimeout(() => { playingfield.appendChild(cardsContainer); }, getRandomIndex(100));
+  }
+};
+
+const changeBet = (direction) => {
+  if (direction == 'up') {
+    if (bet < 5) {
+      bet += 1;
+      datafield.innerHTML = `The current pot is ${pot - bet}. The bet amount is ${bet}.`;
+    }
+  } else if (direction == 'down') {
+    if (bet > 0) {
+      bet -= 1;
+      datafield.innerHTML = `The current pot is ${pot - bet}. The bet amount is ${bet}.`;
+    }
+  }
+};
+
+const initGame = () => {
+// initialise game by generating a shuffled deck and dealing five cards to the player
+  playingfield.innerHTML = '';
+  playerHand = [];
+  cardsToBeSwapped = [];
+  deck = [];
+  cardSuitTally = {};
+  cardValueTally = {};
+
+  generateDeck();
+  shuffledDeck = shuffleCards(deck);
+  playerHand = shuffledDeck.splice(0, 5);
+  pot -= bet;
+
+  swap.disabled = false;
+  betUp.disabled = true;
+  betDown.disabled = true;
+  deal.disabled = true;
+
+  createCardsFromArray(playerHand);
+};
+
+const endTurn = () => {
+  swap.disabled = true;
+  betUp.disabled = false;
+  betDown.disabled = false;
+  deal.disabled = false;
+};
+
+// async loading to ensure html is parsed before DOM selection occurs
+window.addEventListener('load', () => {
+  const playingfield = document.getElementById('playingfield');
+  const datafield = document.getElementById('datafield');
+  datafield.innerHTML = `The current pot is ${pot}. The bet amount is ${bet}.`;
+
+  betUp = document.getElementById('betup');
+  betUp.addEventListener('click', () => { changeBet('up'); });
+  betDown = document.getElementById('betdown');
+  betDown.addEventListener('click', () => { changeBet('down'); });
+
+  deal = document.getElementById('deal');
+  deal.addEventListener('click', initGame);
+
+  swap = document.getElementById('swap');
+  swap.addEventListener('click', () => { swapCards(cardsToBeSwapped, playerHand); });
+});
--- testHands.js
+++ testHands.js
@@ -1,85 +1,127 @@
+const multiplier = {
+  royalFlush: 250,
+  straightFlush: 50,
+  fourOfAKind: 25,
+  fullHouse: 9,
+  flush: 6,
+  straight: 4,
+  threeOfAKind: 3,
+  twoPairs: 2,
+  jacksOrBetter: 1,
+};
+
+// tally the respective values of the hand for counting
+const tallyHandValue = (hand) => {
+  for (let i = 0; i < hand.length; i += 1) {
+    // if we have seen the card __ before, increment its count
+    const cardValue = hand[i].value;
+    if (cardValue in cardValueTally) {
+      cardValueTally[cardValue] += 1;
+    }
+    // else, initialise count of this card __ to 1
+    else {
+      cardValueTally[cardValue] = 1;
+    }
+  }
+
+  for (cardValue in cardValueTally) {
+    console.log(`There are ${cardValueTally[cardValue]} ${cardValue} in the hand`);
+  }
+};
+
+// tally the suits of the hands for counting
+const tallyHandSuit = (hand) => {
+  for (let i = 0; i < hand.length; i += 1) {
+    // if we have seen the card __ before, increment its count
+    const cardSuit = hand[i].suit;
+    if (cardSuit in cardSuitTally) {
+      cardSuitTally[cardSuit] += 1;
+    }
+    // else, initialise count of this card __ to 1
+    else {
+      cardSuitTally[cardSuit] = 1;
+    }
+  }
+
+  for (cardSuit in cardSuitTally) {
+    console.log(`There are ${cardSuitTally[cardSuit]} ${cardSuit} in the hand`);
+  }
+};
+
 // checks a sorted hand to see if the values are consecutive
 const checkConsecutiveValue = (hand) => {
-  for (let i=0 ; i <= hand.length-2; i+=1){
-    if(hand[i+1].value-hand[i].value==1){
-      checkConsecutiveValue(hand.slice(i+1,hand.length));
+  for (let i = 0; i <= hand.length - 2; i += 1) {
+    if (hand[i + 1].value - hand[i].value == 1) {
+      checkConsecutiveValue(hand.slice(i + 1, hand.length));
     } else {
-    return false;}
-  }
-  return true;
-}
-
-// checks a sorted hand to see if the ranks are consecutive
-const checkConsecutiveRank = (hand) => {
-  for (let i=0 ; i <= hand.length-2; i+=1){
-    if(hand[i+1].rank-hand[i].rank==1){
-      checkConsecutiveRank(hand.slice(i+1,hand.length));
-    } else {
-    return false;
+      return false;
     }
   }
   return true;
-}
-
-const checkOfAKind = (handTally) => {
- const noOFsameValues = Object.values(handTally);
- if (noOFsameValues.includes(4)){
-   return 'four of a kind';
-} else if (noOFsameValues.includes(3) && noOFsameValues.includes(2)){
-  return 'full house';
-} else if (noOFsameValues.includes(3)) {
-  return 'three of a kind';
-} else if (noOFsameValues.includes(2)) {
-  const arrayofpairs = noOFsameValues.filter((pairs) => pairs == 2);
-  if (arrayofpairs.length==2){
-    return 'two pairs';
-  } else {
-    return 'pair';
-  }
-}
-}
+};
 
 // checks a sorted hand for same suits
 const checkFlush = (handTally) => {
- const noOFsameSuits = Object.values(handTally);
- if (noOFsameSuits.includes('5')){
-   return true;
-} return false;
-}
+  const noOFsameSuits = Object.values(handTally);
+  if (noOFsameSuits.includes('5')) {
+    return true;
+  } return false;
+};
 
 const calcHandScore = (hand) => {
-  tallyHandSuit(hand);
-  tallyHandValue(hand);
+  // helper function to help sort the cards
+  const comparison = (a, b) => a.rank - b.rank;
+  // Sorts the playerHand by order of rank for calculation
+  const sortedHand = hand.sort(comparison);
+
+  tallyHandSuit(sortedHand);
+  tallyHandValue(sortedHand);
 
   // consecutive value implies royal flush, straight or straight flush
 
-  if(checkConsecutiveValue(hand)){
-    if(checkFlush(cardSuitTally)){
-      if(hand[hand.length-1].value == 13){
-        console.log('Royal Flush!');
-      } else {
-        console.log('Straight Flush!')
+  if (checkConsecutiveValue(sortedHand)) {
+    if (checkFlush(cardSuitTally)) {
+      if (sortedHand[sortedHand.length - 1].value == 13) {
+        if (bet == 5) {
+          pot += 4000;
+        } else {
+          pot += bet * multiplier.royalFlush;
+        }
+        return 'Royal Flush!' + ` Pot has ${pot}`;
       }
-    } else {
-      console.log('Straight!');
+      pot += bet * multiplier.straightFlush;
+      return 'Straight Flush!' + ` Pot has ${pot}`;
     }
-  } else if (checkOfAKind(cardValueTally) === 'four of a kind'){
-    console.log('Four of a Kind!')
-  } else if (checkOfAKind(cardValueTally) === 'full house'){
-    console.log('Full House!')
-  } else if (checkOfAKind(cardValueTally) === 'three of a kind'){
-    console.log('Three of a Kind!')
-  } else if (checkOfAKind(cardValueTally) === 'two pairs'){
-    console.log('Two Pairs!');
-  } else if (checkOfAKind(cardValueTally) === 'pair'){
-    const pairsValue = Object.keys(cardValueTally).find(key => cardValueTally[key] === 2);
-    if(pairsValue >= 11){
-      console.log('Jacks or Better!')
-    } else {
-      console.log('Single Pair!');
-    }
-  } else {
-    console.log('High Card!');
+    pot += bet * multiplier.straight;
+    return 'Straight!' + ` Pot has ${pot}`;
   }
 
+  const noOFsameValues = Object.values(cardValueTally);
+  if (noOFsameValues.includes(4)) {
+    pot += bet * multiplier.fourOfAKind;
+    return 'Four of a kind!' + ` Pot has ${pot}`;
+  } if (noOFsameValues.includes(3) && noOFsameValues.includes(2)) {
+    pot += bet * multiplier.fullHouse;
+    return 'Full house!' + ` Pot has ${pot}`;
+  } if (noOFsameValues.includes(3)) {
+    pot += bet * multiplier.threeOfAKind;
+    return 'Three of a kind!' + ` Pot has ${pot}`;
+  } if (noOFsameValues.includes(2)) {
+    const arrayofpairs = noOFsameValues.filter((pairs) => pairs == 2);
+    if (arrayofpairs.length == 2) {
+      pot += bet * multiplier.twoPairs;
+      return 'Two pairs!' + ` Pot has ${pot}`;
+    }
+    const pairsValue = Object.keys(cardValueTally).find((key) => cardValueTally[key] === 2);
+    if (pairsValue >= 11) {
+      pot += bet * multiplier.jacksOrBetter;
+      return 'Jacks or Better!' + ` Pot has ${pot}`;
+    }
+    return 'Single Pair!' + ` Pot has ${pot}`;
   }
+  return 'High Card!' + ` Pot has ${pot}`;
+};
+
+const highlightWinners = () => {
+
+};
